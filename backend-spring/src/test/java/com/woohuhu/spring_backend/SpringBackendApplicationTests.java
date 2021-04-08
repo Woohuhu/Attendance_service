e2ac0ee69e46bfb26521bdde33f091b021e13138
Hwangsoomin <maum97@naver.com>
2021-04-08 19:08:45 +0900

    [#2] feat - spring boot backend 초기화

---
+++ backend-spring/src/test/java/com/woohuhu/spring_backend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woohuhu.spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
