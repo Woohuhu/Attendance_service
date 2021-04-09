e2050ad9948b48fc5f3204ed379e4da63b570410
hschoi1104 <hschoi1104@gmail.com>
2021-04-10 01:14:32 +0900

    [#3] feat - UserNotFoundException 수정

--- backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/exception/UserNotFoundException.java
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/exception/UserNotFoundException.java
@@ -4,7 +4,7 @@ public class UserNotFoundException extends RuntimeException {
 
     private static final String USER_NOT_FOUND_EXCEPTION_MESSAGE = "해당하는 유저를 찾을 수 없습니다.";
 
-    UserNotFoundException() {
+    public UserNotFoundException() {
         super(USER_NOT_FOUND_EXCEPTION_MESSAGE);
     }
 }
