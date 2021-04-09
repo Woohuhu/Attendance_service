fd4025b7d084a980bf368a12527253289b45f842
hschoi1104 <hschoi1104@gmail.com>
2021-04-10 01:06:23 +0900

    [#3] feat - UserDto에 Validation 추가

--- backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/dto/UserDto.java
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/dto/UserDto.java
@@ -2,12 +2,19 @@ package com.woohuhu.spring_backend_auth.user.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
 
 public class UserDto {
+    @NotEmpty
+    @NotBlank
+    @NotNull
     private String id;
     private String password;
     private String name;
