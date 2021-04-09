a5a6c39c2b6eb3aaae50278f0fda7e9b14a8f302
hschoi1104 <hschoi1104@gmail.com>
2021-04-10 01:15:01 +0900

    [#3] feat - UserService 수정

--- backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/service/UserServiceImpl.java
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.woohuhu.spring_backend_auth.user.service;
 
 import com.woohuhu.spring_backend_auth.user.dao.UserDao;
+import com.woohuhu.spring_backend_auth.user.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.woohuhu.spring_backend_auth.user.dto.UserDto;
+
+import static java.util.Objects.isNull;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -13,6 +16,10 @@ public class UserServiceImpl implements UserService{
     @Override
     public UserDto getUser(String id) throws Exception{
         UserDto userDto = userDao.getUser(id);
+
+        if(isNull(userDto)){
+            throw new UserNotFoundException();
+        }
         return userDto;
     }
 }
