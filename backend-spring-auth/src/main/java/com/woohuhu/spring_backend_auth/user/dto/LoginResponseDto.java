3bc5e82ce70f9c7d7d62f7705f8036b78809584d
Hwangsoomin <maum97@naver.com>
2021-04-12 03:07:01 +0900

    [#8] feat - LoginResponseDto.java 생성

---
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/dto/LoginResponseDto.java
@@ -0,0 +1,21 @@
+package com.woohuhu.spring_backend_auth.user.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+
+public class LoginResponseDto {
+    @NotEmpty @NotBlank @NotNull
+    private String accessToken;
+    @NotEmpty @NotBlank @NotNull
+    private String id;
+    @NotEmpty @NotBlank @NotNull
+    private String name;
+}
