dbcb37a03960c658ace033d31bb72055cbb9c64e
hschoi1104 <hschoi1104@gmail.com>
2021-04-13 01:30:20 +0900

    [#10] feat - UserDao.java 수정

--- backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/dao/UserDao.java
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/dao/UserDao.java
@@ -1,10 +1,14 @@
 package com.woohuhu.spring_backend_auth.user.dao;
 
+import com.woohuhu.spring_backend_auth.user.dto.RefreshTokenDto;
 import com.woohuhu.spring_backend_auth.user.dto.UserDto;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao {
     UserDto getUser(String id) throws Exception;
+
     int createUser(UserDto userDto) throws Exception;
+
+    RefreshTokenDto createRefreshToken(RefreshTokenDto refreshTokenDto) throws Exception;
 }
