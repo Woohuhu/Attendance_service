f72c6ab12493397a593d6cb5f73599079fafad11
Hwangsoomin <maum97@naver.com>
2021-04-13 17:26:59 +0900

    [#12] fix - JWTService 수정

--- backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/global/service/JWTService.java
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/global/service/JWTService.java
@@ -1,9 +1,9 @@
 package com.woohuhu.spring_backend_auth.global.service;
 
-import com.woohuhu.spring_backend_auth.user.dto.UserDto;
+import com.woohuhu.spring_backend_auth.user.dto.*;
 
 public interface JWTService {
-    String generateAccessToken(UserDto userDto) throws Exception;
+    String generateAccessToken(UserInfoDto userInfoDto) throws Exception;
 
     String generateRefreshToken(String id) throws Exception;
 
