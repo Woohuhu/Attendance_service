85b91363724066c42a36ac6ad7af74ecf993a972
Hwangsoomin <maum97@naver.com>
2021-04-13 15:40:03 +0900

    [#10] feat - JWTService 수정

--- backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/global/service/JWTService.java
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/global/service/JWTService.java
@@ -4,5 +4,10 @@ import com.woohuhu.spring_backend_auth.user.dto.UserDto;
 
 public interface JWTService {
     String generateAccessToken(UserDto userDto) throws Exception;
+
     String generateRefreshToken(String id) throws Exception;
+
+    String verifyAccessToken(String accessToken) throws Exception;
+
+    String verifyRefreshToken(String refreshToken) throws Exception;
 }
--- backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/global/service/JWTServiceImpl.java
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/global/service/JWTServiceImpl.java
@@ -1,11 +1,16 @@
 package com.woohuhu.spring_backend_auth.global.service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.woohuhu.spring_backend_auth.user.dto.UserDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class JWTServiceImpl implements JWTService {
@@ -13,16 +18,25 @@ public class JWTServiceImpl implements JWTService {
     @Value("${AccessTokenSecretKey}")
     private String AccessTokenSecretKey;
 
-    @Value("${RefreshTokenSecretKey")
+    @Value("${RefreshTokenSecretKey}")
     private String RefreshTokenSecretKey;
+
+    @Value("${AccessTokenExpires}")
+    private Long AccessTokenExpires;
+
+    @Value("${RefreshTokenExpires}")
+    private Long RefreshTokenExpires;
 
     @Override
     public String generateAccessToken(UserDto userDto) {
         try {
+            Date ext = new Date();
+            ext.setTime(ext.getTime() + AccessTokenExpires);
             Algorithm algorithm = Algorithm.HMAC256(AccessTokenSecretKey);
             String accessToken = JWT.create()
                     .withClaim("id", userDto.getId())
                     .withClaim("name", userDto.getName())
+                    .withExpiresAt(ext)
                     .sign(algorithm);
             return accessToken;
         } catch (JWTCreationException exception) {
@@ -33,12 +47,43 @@ public class JWTServiceImpl implements JWTService {
     @Override
     public String generateRefreshToken(String id) {
         try {
+            Date ext = new Date();
+            ext.setTime(ext.getTime() + RefreshTokenExpires);
             Algorithm algorithm = Algorithm.HMAC256(RefreshTokenSecretKey);
             String refreshToken = JWT.create()
                     .withClaim("id", id)
+                    .withExpiresAt(ext)
                     .sign(algorithm);
             return refreshToken;
         } catch (JWTCreationException exception) {
+            throw exception;
+        }
+    }
+
+    @Override
+    public String verifyAccessToken(String accessToken) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(AccessTokenSecretKey);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .acceptExpiresAt(AccessTokenExpires)
+                    .build();
+            DecodedJWT jwt = verifier.verify(accessToken);
+            return jwt.toString();
+        } catch (JWTVerificationException exception) {
+            throw exception;
+        }
+    }
+
+    @Override
+    public String verifyRefreshToken(String refreshToken) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(RefreshTokenSecretKey);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .acceptExpiresAt(RefreshTokenExpires)
+                    .build();
+            DecodedJWT jwt = verifier.verify(refreshToken);
+            return jwt.toString();
+        } catch (JWTVerificationException exception) {
             throw exception;
         }
     }
