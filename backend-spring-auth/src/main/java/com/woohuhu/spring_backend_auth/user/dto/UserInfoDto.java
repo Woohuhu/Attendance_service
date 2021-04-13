ae9b1fc22e7b0105eb7f3390cc6bd2e25373762e
Hwangsoomin <maum97@naver.com>
2021-04-13 17:28:47 +0900

    [#12] feat - UserInfoDto.java 생성

---
+++ backend-spring-auth/src/main/java/com/woohuhu/spring_backend_auth/user/dto/UserInfoDto.java
@@ -0,0 +1,23 @@
+package com.woohuhu.spring_backend_auth.user.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+
+public class UserInfoDto {
+    @NotEmpty
+    @NotBlank
+    @NotNull
+    private String id;
+    @NotEmpty
+    @NotBlank
+    @NotNull
+    private String name;
+}
