375126617f9017f3e37f9b88d1153dc4f2619c58
Hwangsoomin <maum97@naver.com>
2021-04-14 17:50:29 +0900

    [#14] feat -  Header.vue 생성

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-app-bar color="white" flat>
+      <v-container class="py-0">
+        <v-avatar class="mr-10" color="teal darken-1" size="32"></v-avatar>
+
+        <v-btn v-for="link in links" :key="link" text>
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+      </v-container>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: ["Dashboard", "Messages", "Profile", "Updates"],
+  }),
+};
+</script>
