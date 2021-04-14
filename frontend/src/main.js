d4fa29b0bf7fd3a14e134c8f42a0790cdd9034e1
hschoi1104 <hschoi1104@gmail.com>
2021-04-14 16:48:41 +0900

    [#2] feat - AxiosAuth 인스턴스 생성

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,10 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueLogger from "vuejs-logger";
+import axiosAuth from "./service/global/axios.auth";
 const isProduction = process.env.NODE_ENV === "production";
 
+Vue.prototype.$axiosAuth = axiosAuth;
 Vue.config.productionTip = false;
 
 const options = {
