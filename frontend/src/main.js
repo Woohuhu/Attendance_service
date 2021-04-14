5ca511a2aa4f72826f2b417b8f58c63ab1d44aae
Hwangsoomin <maum97@naver.com>
2021-04-14 16:26:29 +0900

    [#2] feat - main.js 수정
    vue-logger 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueLogger from "vuejs-logger";
+const isProduction = process.env.NODE_ENV === "production";
 
 Vue.config.productionTip = false;
+
+const options = {
+  isEnabled: true,
+  logLevel: isProduction ? "error" : "debud",
+  stringifyArguments: false,
+  showLogLevel: true,
+  showMethodName: true,
+  separator: "|",
+  showConsoleColors: true,
+};
+
+Vue.use(VueLogger, options);
 
 new Vue({
   router,
