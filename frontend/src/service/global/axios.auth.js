2706ea04b262e6508be72a75375f753c864f84e1
hschoi1104 <hschoi1104@gmail.com>
2021-04-15 18:22:14 +0900

    [#16] fix - axios.auth.js
    NODE_ENV 환경변수 조건 변경

--- frontend/src/service/global/axios.auth.js
+++ frontend/src/service/global/axios.auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosAuth = axios.create({
   baseURL:
-    process.env.VUE_APP_NODE_ENV == "dev"
+    process.env.VUE_APP_NODE_ENV != "production"
       ? "http://localhost:8080/api"
       : process.env.VUE_APP_AUTH_URL,
   withCredentials: true,
