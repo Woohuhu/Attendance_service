1ffaa8f517e0bbb3f958c13d311e2bfec68efabe
Hwangsoomin <maum97@naver.com>
2021-04-15 16:46:19 +0900

    [#16] feat - store/index.js 수정

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  plugins: [createPersistedState()],
+  state: {
+    id: null,
+    name: null,
+    accesstoken: null,
+  },
+  mutations: {
+    Login(state, user) {
+      state.id = user.id;
+      state.name = user.name;
+      state.accesstoken = user.accesstoken;
+    },
+  },
+  actions: {
+    async Login(context, { id, password }) {
+      const result = await this.$axiosAuth.post("/v1/login", {
+        id,
+        password,
+      });
+      const user = result.data;
+      return context.commit("Login", user);
+    },
+  },
   modules: {},
 });
