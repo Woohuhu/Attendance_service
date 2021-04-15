f43a14c537d3387261871ecb99b443ca62d8b63b
hschoi1104 <hschoi1104@gmail.com>
2021-04-15 17:43:30 +0900

    [#16] fix - axios.auth.js 수정

--- frontend/src/service/global/axios.auth.js
+++ frontend/src/service/global/axios.auth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosAuth = axios.create({
   baseURL:
     process.env.VUE_APP_NODE_ENV == "dev"
-      ? "http://localhost:3000/api"
+      ? "http://localhost:8080/api"
       : process.env.VUE_APP_AUTH_URL,
   withCredentials: true,
 });
