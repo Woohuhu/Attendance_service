6230a632b3841272fc6cc5d0e75a1966afccaf98
Hwangsoomin <maum97@naver.com>
2021-04-15 14:46:41 +0900

    [#14] feat - Header.vue 수정
    url 연결

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,11 +2,34 @@
   <div>
     <v-app-bar color="white" flat>
       <v-container class="py-0">
-        <v-avatar class="mr-10" color="teal darken-1" size="32"></v-avatar>
+        <v-row align="center" justify="center">
+          <v-col cols="8">
+            <v-avatar
+              class="mr-10 justify-center"
+              color="teal darken-1"
+              size="32"
+            ></v-avatar>
+            <v-btn
+              v-for="link in links_left"
+              :key="link.name"
+              text
+              @click="$router.push(link.url)"
+            >
+              {{ link.name }}
+            </v-btn>
+          </v-col>
 
-        <v-btn v-for="link in links" :key="link" text>
-          {{ link }}
-        </v-btn>
+          <v-col class="text-right" cols="4">
+            <v-btn
+              v-for="link in links_right"
+              :key="link.name"
+              text
+              @click="$router.push(link.url)"
+            >
+              {{ link.name }}
+            </v-btn>
+          </v-col>
+        </v-row>
 
         <v-spacer></v-spacer>
       </v-container>
@@ -17,7 +40,30 @@
 <script>
 export default {
   data: () => ({
-    links: ["Dashboard", "Messages", "Profile", "Updates"],
+    links_left: [
+      {
+        name: "출석체크",
+        url: "/attendance",
+      },
+      {
+        name: "출석현황(일별)",
+        url: "/attendance",
+      },
+      {
+        name: "출석현황(사용자별)",
+        url: "/attendance",
+      },
+    ],
+    links_right: [
+      {
+        name: "회원가입",
+        url: "/signup",
+      },
+      {
+        name: "로그인",
+        url: "/login",
+      },
+    ],
   }),
 };
 </script>
