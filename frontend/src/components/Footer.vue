5247a34dc9e98e7287d558120608d302e4f6be47
hschoi1104 <hschoi1104@gmail.com>
2021-04-15 14:53:29 +0900

    [#14] feat - Footer.vue 수정

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <v-app-bar flat color="white" dark dense></v-app-bar>
+    <v-app-bar flat color="white" dark dense>
+      <v-col align="center" justify="center">
+        <v-btn text color="black" @click="onClickRedirect()">
+          🌱 Woohuhu github 🌱
+        </v-btn>
+      </v-col>
+    </v-app-bar>
   </div>
 </template>
+<script>
+export default {
+  data: () => ({}),
+  methods: {
+    onClickRedirect: function () {
+      window.open("https://github.com/Woohuhu/Attendance_service", "_blank");
+    },
+  },
+};
+</script>
