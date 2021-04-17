e707987da3ee0a89500aed573fd6bf7ef865bdd3
hschoi1104 <hschoi1104@gmail.com>
2021-04-17 18:15:18 +0900

    [#20] feat - Response.java 생성

---
+++ backend-spring/src/main/java/com/woohuhu/spring_backend/global/dto/Response.java
@@ -0,0 +1,34 @@
+package com.woohuhu.spring_backend.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Response<T> {
+    private int statusCode;
+    private String message;
+    private T data;
+
+    public Response(final int statusCode, final String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = null;
+    }
+
+    public static <T> Response<T> response(final int statusCode, final String message) {
+        return response(statusCode, message, null);
+    }
+
+    public static <T> Response<T> response(final int statusCode, final String message, final T data) {
+        return Response.<T>builder()
+                .statusCode(statusCode)
+                .message(message)
+                .data(data)
+                .build();
+    }
+}
