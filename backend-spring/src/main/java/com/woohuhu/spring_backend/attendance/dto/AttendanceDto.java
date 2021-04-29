66f32ab16998fa08cbaec4d4fb24cc76eeca457f
hschoi1104 <hschoi1104@gmail.com>
2021-04-29 17:10:22 +0900

    [#20] feat - AttendanceDto.java 수정

--- backend-spring/src/main/java/com/woohuhu/spring_backend/attendance/dto/AttendanceDto.java
+++ backend-spring/src/main/java/com/woohuhu/spring_backend/attendance/dto/AttendanceDto.java
@@ -8,18 +8,9 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Builder
-@Entity
-@Table(name = "attendance")
 public class AttendanceDto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private String id;
-
-    @Temporal(TemporalType.TIMESTAMP)
     private java.util.Date date;
-
-    @Column
     private String state;
 
 
