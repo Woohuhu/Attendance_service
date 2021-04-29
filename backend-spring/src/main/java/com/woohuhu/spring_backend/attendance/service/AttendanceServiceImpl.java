60cf50a6ca67c8e94d3e1d8d1c89efe98a7f9061
hschoi1104 <hschoi1104@gmail.com>
2021-04-29 17:10:05 +0900

    [#20] feat - AttendanceServiceImpl.java 수정

--- backend-spring/src/main/java/com/woohuhu/spring_backend/attendance/service/AttendanceServiceImpl.java
+++ backend-spring/src/main/java/com/woohuhu/spring_backend/attendance/service/AttendanceServiceImpl.java
@@ -1,23 +1,26 @@
 package com.woohuhu.spring_backend.attendance.service;
 
+import com.woohuhu.spring_backend.attendance.Entity.Attendance;
 import com.woohuhu.spring_backend.attendance.dao.AttendanceRepository;
 import com.woohuhu.spring_backend.attendance.dto.AttendanceDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
 
 @RequiredArgsConstructor
 @Service
 public class AttendanceServiceImpl implements AttendanceService {
     private final AttendanceRepository attendanceRepository;
 
-    @Override
-    public AttendanceDto createAttendance(AttendanceDto attendanceDto) throws Exception {
-        AttendanceDto attendance = AttendanceDto.builder()
-                .state(attendanceDto.getState())
-                .id(attendanceDto.getId())
-                .date(attendanceDto.getDate())
-                .build();
-
-        return  attendanceRepository.save(attendance);
-    }
+@Override
+public AttendanceDto createAttendance(AttendanceDto attendanceDto) throws Exception {
+    Attendance attendance = Attendance.builder()
+            .state(attendanceDto.getState())
+            .id(attendanceDto.getId())
+            .date(new Timestamp(attendanceDto.getDate().getTime()))
+            .build();
+    attendanceRepository.save(attendance);
+    return  attendanceDto;
+}
 }
