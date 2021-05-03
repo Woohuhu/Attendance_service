8a330dad0f7505bcc05cbe67fbcb62e0af2bbfaa
hschoi1104 <hschoi1104@gmail.com>
2021-05-04 01:28:27 +0900

    [#24] feat - AttendanceRepository.java 수정

--- backend-spring/src/main/java/com/woohuhu/spring_backend/attendance/dao/AttendanceRepository.java
+++ backend-spring/src/main/java/com/woohuhu/spring_backend/attendance/dao/AttendanceRepository.java
@@ -2,12 +2,17 @@ package com.woohuhu.spring_backend.attendance.dao;
 
 import com.woohuhu.spring_backend.attendance.Entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
     List<Attendance> findAllById(@Param("id") String id);
+
+    @Query(value = "SELECT attendance.* FROM attendance WHERE date =:date", nativeQuery = true)
+    List<Attendance> findAllByDate(LocalDate date);
 }
