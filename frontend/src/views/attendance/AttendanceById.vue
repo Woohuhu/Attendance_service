083c5e08e1ae6de262a9c90db8c8e232947916fc
Hwangsoomin <maum97@naver.com>
2021-05-09 02:33:35 +0900

    [#31] feat - AttendanceById.vue 생성

---
+++ frontend/src/views/attendance/AttendanceById.vue
@@ -0,0 +1,18 @@
+<template>
+  <ByIdComponent v-bind:attendance="attendances"></ByIdComponent>
+</template>
+
+<script>
+import ByIdComponent from "../../components/attendance/ByIdComponent";
+
+export default {
+  components: {
+    ByIdComponent,
+  },
+  data: () => ({
+    attendances: [],
+    name: "",
+  }),
+  async created() {},
+};
+</script>
