9a172be8ea6dfa31883400767b7c6f4c53c6a3b2
Hwangsoomin <maum97@naver.com>
2021-05-10 17:51:36 +0900

    [#35] feat - UserListComponent.vue 생성

---
+++ frontend/src/components/attendance/UserListComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="mx-auto" max-width="130" max-height="130" outlined>
+    <v-img
+      src="https://user-images.githubusercontent.com/43667316/117630005-b7749580-b1b5-11eb-93c3-5d8938182cb4.JPG"
+      width="130px"
+      height="95px"
+    ></v-img>
+    <v-list-item one-line>
+      <v-list-item-content>
+        <v-list-item-title align="center" class="title mb-1">
+          <span>{{ userInfo.name }}</span>
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["userInfo"],
+};
+</script>
