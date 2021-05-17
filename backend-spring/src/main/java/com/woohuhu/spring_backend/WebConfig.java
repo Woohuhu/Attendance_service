e2f83543f070faba55250d2c99204acee9b2c7e6
hschoi1104 <hschoi1104@gmail.com>
2021-05-18 01:37:01 +0900

    [#35] feat - WebConfig.java 수정

--- backend-spring/src/main/java/com/woohuhu/spring_backend/WebConfig.java
+++ backend-spring/src/main/java/com/woohuhu/spring_backend/WebConfig.java
@@ -3,6 +3,7 @@ package com.woohuhu.spring_backend;
 import com.woohuhu.spring_backend.global.interceptor.JwtInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,11 +11,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     private static final String[] EXCLUDE_PATHS = {
             "/login/**",
-            "/signup/**"
+            "/signup/**",
+            "/socket/**"
     };
 
     @Autowired
     private JwtInterceptor jwtInterceptor;
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
